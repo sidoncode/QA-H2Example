016f180b9c6b4f9414301d49dd3ff7146ef178ed
sidoncode <siddharth54330@gmail.com>
2022-09-08 19:57:04 +0530

    h2 console with - schema and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO CITY (ID,city_code, city_name, city_pincode)
+VALUES (1,'ABCD', 'EFGH', 420);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS CITY;
+CREATE TABLE CITY (
+    ID int NOT NULL,
+    city_code varchar(255) NOT NULL,
+    city_name varchar(255),
+    city_pincode int
+);
+
